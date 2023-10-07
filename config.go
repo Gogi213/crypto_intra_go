cac675d6e58b2bf0602ab2ad3a55506680ced778
Redmi <1993georgiy@gmail.com>
2023-10-07 16:11:43 +0300

    stage config 6 inst

--- config.go
+++ config.go
@@ -3,20 +3,48 @@ package main
 
 var Instance1 = Config{
 	APIKeys:        []string{"jByRuDyDvM3bQl71hLgadWt932jodjvpJRqvXsQRIWHfpSZwxYBR7BWFBOXO7o6b", "ceox9ksHbEyMQiOkSkH0k2VnFWkiojJEucGIMRUZXUq3dLCuOqV81nQIjCh0PLK5"},
-	ProxyAddresses: []string{"212.115.48.122:13225", "195.225.96.39:13225"},
+	ProxyAddresses: []string{"176.56.38.141:13041", "176.56.38.38:13041"},
 	PairsList: [][]string{
-		{"1inchbtc", "1inchbusd", "1inchusdt", "aavebnb", "aavebtc", "aavebusd", "aaveusdt", "acabtc", "acabusd", "acatry", "acausdt", "achbtc", "achbusd", "achtry", "achusdt", "acmbusd", "acmusdt", "adabnb", "adabrl", "adabtc", "adabusd", "adaeth", "adaeur", "adatry", "adausdt", "adadownusdt", "adaupusdt", "adxusdt", "aergousdt", "agixbtc", "agixbusd", "agixtry", "agixusdt", "agldbtc", "agldbusd", "agldusdt", "akrousdt", "alcxusdt", "algobtc", "algobusd", "algotry", "algousdt", "alicebusd", "aliceusdt", "alpacabtc", "alpacabusd", "alpacausdt", "alphausdt", "alpinebusd", "alpinetry", "alpineusdt", "ambbusd", "ambusdt", "ampbusd", "ampusdt", "ankrbtc", "ankrbusd", "ankrtry", "ankrusdt", "antusdt", "apebtc", "apebusd", "apetry", "apeusdt", "api3usdt", "aptbtc", "aptbusd", "apttry", "aptusdt", "arbtc", "arusdt", "arbbtc", "arbeth", "arbtry", "arbtusd", "arbusdt", "ardrusdt", "arkbusd", "arkmbnb", "arkmbtc"},
-		{"arkmtry", "arkmtusd", "arkmusdt", "arpabusd", "arpatry", "arpausdt", "asrusdt", "astbtc", "astusdt", "astrbtc", "astrbusd", "astrusdt", "atausdt", "atmbusd", "atmusdt", "atombtc", "atombusd", "atometh", "atomeur", "atomtry", "atomusdt", "auctionbtc", "auctionbusd", "auctionusdt", "audiotry", "audiousdt", "avabtc", "avausdt", "avaxbnb", "avaxbtc", "avaxbusd", "avaxeth", "avaxeur", "avaxtry", "avaxusdt", "axsbtc", "axsbusd", "axsusdt", "badgerusdt", "bakebusd", "bakeusdt", "balusdt", "bandbusd", "bandusdt", "barbusd", "barusdt", "batbusd", "batusdt", "bchbnb", "bchbtc", "bchbusd", "bcheur", "bchtry", "bchusdt", "bdotdot", "belbusd", "beltry", "belusdt", "betausdt", "betheth", "bethusdt", "bicousdt", "bifiusdt", "blzbtc", "blzusdt", "bnbbidr", "bnbbrl", "bnbbtc", "bnbbusd", "bnbeth", "bnbeur", "bnbfdusd", "bnbgbp", "bnbtry", "bnbtusd", "bnbusdc", "bnbusdt", "bnbdownusdt", "bnbupusdt", "bntusdt"},
-		{"bnxusdt", "bondbusd", "bondusdt", "bswbusd", "bswtry", "bswusdt", "btcars", "btcbidr", "btcbrl", "btcbusd", "btcdai", "btceur", "btcgbp", "btcngn", "btcpln", "btcrub", "btctry", "btctusd", "btcusdc", "btcusdt", "btczar", "btcdownusdt", "btcupusdt", "btsusdt", "bttctry", "bttcusdt", "burgerbusd", "burgerusdt", "busdbidr", "busdbrl", "busddai", "busdpln", "busdrub", "busdtry", "busdusdt", "busdzar", "c98usdt", "cakebnb", "cakebusd", "cakeusdt", "celobtc", "celobusd", "celousdt", "celrbusd", "celrusdt", "cfxbtc", "cfxbusd", "cfxtry", "cfxusdt", "chessbtc", "chessbusd", "chessusdt", "chrbtc", "chrbusd", "chrusdt", "chzbtc", "chzbusd", "chztry", "chzusdt", "citybusd", "citytry", "cityusdt", "ckbusdt", "clvbtc", "clvbusd", "clvusdt", "combotry", "combousdt", "compbtc", "compbusd", "comptry", "compusdt", "cosbtc", "costry", "cosusdt", "cotibusd", "cotiusdt", "creambusd", "crvbtc", "crvbusd"},
+		{"1inchbtc", "1inchbusd"},
 	},
 }
 
 var Instance2 = Config{
-	APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
-	ProxyAddresses: []string{"193.160.211.65:13225", "45.86.3.128:13225"},
+	APIKeys:        []string{"jByRuDyDvM3bQl71hLgadWt932jodjvpJRqvXsQRIWHfpSZwxYBR7BWFBOXO7o6b", "ceox9ksHbEyMQiOkSkH0k2VnFWkiojJEucGIMRUZXUq3dLCuOqV81nQIjCh0PLK5"},
+	ProxyAddresses: []string{"176.56.38.141:13041", "176.56.38.38:13041"},
 	PairsList: [][]string{
-		{"crvusdt", "ctkusdt", "ctsiusdt", "ctxcbusd", "ctxcusdt", "cvcusdt", "cvpbusd", "cvpusdt", "cvxbtc", "cvxbusd", "cvxusdt", "darbusd", "dartry", "darusdt", "dashbtc", "dashusdt", "datausdt", "dcrusdt", "degobtc", "degousdt", "dentbusd", "denttry", "dentusdt", "dexeusdt", "dfusdt", "dgbbtc", "dgbbusd", "dgbusdt", "diabtc", "diabusd", "diausdt", "dockusdt", "dodobusd", "dodousdt", "dogebidr", "dogebrl", "dogebtc", "dogebusd", "dogeeur", "dogegbp", "dogetry", "dogetusd", "dogeusdt", "dotbtc", "dotbusd", "doteur", "dottry", "dotusdt", "drepbusd", "drepusdt", "duskbusd", "duskusdt", "dydxbtc", "dydxbusd", "dydxusdt", "edutry", "eduusdt", "egldbtc", "egldbusd", "egldeur", "egldusdt", "elfbtc", "elfbusd", "elfeth", "elfusdt", "enjbtc", "enjbusd", "enjtry", "enjusdt", "ensusdt", "eosbtc", "eosbusd", "eosusdt", "epxusdt", "ernusdt", "etcbtc", "etcbusd", "etcusdt", "ethbidr", "ethbrl"},
-		{"ethbtc", "ethbusd", "ethdai", "etheur", "ethgbp", "ethpln", "ethrub", "ethtry", "ethtusd", "ethusdc", "ethusdt", "ethzar", "ethdownusdt", "ethupusdt", "farmusdt", "fdusdbusd", "fdusdusdt", "fetbtc", "fetbusd", "fettry", "fetusdt", "fidabtc", "fidabusd", "fidausdt", "filbtc", "filbusd", "fileth", "filtry", "filusdt", "fiousdt", "firobtc", "firobusd", "firousdt", "fisbtc", "fisusdt", "flmusdt", "flokitry", "flokiusdt", "flowbnb", "flowbtc", "flowbusd", "flowusdt", "fluxusdt", "forusdt", "forthusdt", "frontbusd", "frontusdt", "ftmbnb", "ftmbtc", "ftmbusd", "ftmeth", "ftmeur", "ftmtry", "ftmusdt", "fttbusd", "funbnb", "funeth", "funusdt", "fxsbusd", "fxsusdt", "galtry", "galusdt", "galabnb", "galabrl", "galabtc", "galabusd", "galaeth", "galaeur", "galatry", "galausdt", "gasbtc", "gasbusd", "gasusdt", "gftbusd", "ghstusdt", "glmusdt", "glmrbusd", "glmrusdt", "gmtbtc", "gmtbusd"},
-		{"gmttry", "gmtusdt", "gmxbusd", "gmxusdt", "gnousdt", "gnsusdt", "grtbtc", "grtbusd", "grttry", "grtusdt", "gtcusdt", "hardusdt", "hbarbtc", "hbarbusd", "hbarusdt", "hftbtc", "hftbusd", "hftusdt", "hifiusdt", "highbusd", "highusdt", "hivebtc", "hiveusdt", "hookbusd", "hookusdt", "hottry", "hotusdt", "icpbtc", "icpbusd", "icpusdt", "icxbusd", "icxusdt", "idbtc", "idtry", "idtusd", "idusdt", "idexbtc", "idexbusd", "idexusdt", "ilvbusd", "ilvusdt", "imxbusd", "imxusdt", "injbnb", "injbtc", "injbusd", "injtry", "injusdt", "iostbtc", "iostusdt", "iotabtc", "iotausdt", "iotxusdt", "iqbusd", "irisusdt", "jasmybusd", "jasmytry", "jasmyusdt", "joebusd", "joetry", "joeusdt", "jstusdt", "juvbusd", "juvusdt", "kavabtc", "kavabusd", "kavausdt", "kdausdt", "keybusd", "keyusdt", "klayusdt", "kmdusdt", "kncbtc", "kncbusd", "kncusdt", "kp3rusdt", "ksmusdt", "laziobusd", "laziotry", "laziousdt"},
+		{"arkmtry", "arkmtusd"},
+	},
+}
+
+var Instance3 = Config{
+	APIKeys:        []string{"jByRuDyDvM3bQl71hLgadWt932jodjvpJRqvXsQRIWHfpSZwxYBR7BWFBOXO7o6b", "ceox9ksHbEyMQiOkSkH0k2VnFWkiojJEucGIMRUZXUq3dLCuOqV81nQIjCh0PLK5"},
+	ProxyAddresses: []string{"176.56.38.141:13041", "176.56.38.38:13041"},
+	PairsList: [][]string{
+		{"bnxusdt", "bondbusd"},
+	},
+}
+
+var Instance4 = Config{
+	APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
+	ProxyAddresses: []string{"176.56.38.204:13041", "176.56.38.246:13041"},
+	PairsList: [][]string{
+		{"crvusdt", "ctkusdt"},
+	},
+}
+
+var Instance5 = Config{
+	APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
+	ProxyAddresses: []string{"176.56.38.204:13041", "176.56.38.246:13041"},
+	PairsList: [][]string{
+		{"ethbtc", "ethbusd"},
+	},
+}
+
+var Instance6 = Config{
+	APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
+	ProxyAddresses: []string{"176.56.38.204:13041", "176.56.38.246:13041"},
+	PairsList: [][]string{
+		{"gmttry", "gmtusdt"},
 	},
 }
