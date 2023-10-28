6c9c128071260a1f6fc206696d422ba0306ab74f
Redmi <1993georgiy@gmail.com>
2023-10-28 21:50:24 +0300

    тесты

--- config.go
+++ config.go
@@ -1,50 +1,64 @@
 // config.go
 package main
 
-var Instance1 = Config{
-	APIKeys:        []string{"jByRuDyDvM3bQl71hLgadWt932jodjvpJRqvXsQRIWHfpSZwxYBR7BWFBOXO7o6b", "ceox9ksHbEyMQiOkSkH0k2VnFWkiojJEucGIMRUZXUq3dLCuOqV81nQIjCh0PLK5"},
-	ProxyAddresses: []string{"185.186.27.185:15421", "185.186.27.184:15421"},
-	PairsList: [][]string{
-		{"1inchusdt", "aavebusd", "aaveusdt", "acatry", "acausdt", "achtry", "achusdt", "acmtry", "acmusdt", "adabidr", "adabrl", "adabusd", "adadownusdt", "adatry", "adaupusdt", "adausdt", "adxusdt", "agixtry", "agixusdt", "agldusdt", "akrousdt", "alcxusdt", "algotry", "algousdt", "alicetry", "aliceusdt", "alpacausdt", "alphausdt", "alpinetry", "alpineusdt", "ambusdt", "amptry", "ampusdt", "ankrtry", "ankrusdt", "antusdt", "apebusd", "apetry", "apeusdt", "api3try", "api3usdt", "apttry", "aptusdt", "arbtry", "arbusdt", "ardrusdt", "arkbusd", "arkmrub", "arkmtry", "arkmusdt", "arkusdt", "arpatry", "arpausdt", "arusdt", "asrtry", "asrusdt", "astrusdt", "atausdt", "atmtry", "atmusdt", "atomtry", "atomusdt", "auctionbusd", "auctionusdt", "audiotry", "audiousdt", "avaxbusd", "avaxtry", "avaxusdt", "axsbidr", "axstry", "axsusdt", "badgerusdt", "bakebusd", "bakeusdt", "balusdt", "bartry", "barusdt", "batusdt", "bchbusd", "bchtry", "bchusdt", "beltry", "belusdt", "betausdt", "bethusdt", "bicobusd", "bicousdt", "blzusdt"},
-	},
-}	
-
-var Instance2 = Config{
-	APIKeys:        []string{"jByRuDyDvM3bQl71hLgadWt932jodjvpJRqvXsQRIWHfpSZwxYBR7BWFBOXO7o6b", "ceox9ksHbEyMQiOkSkH0k2VnFWkiojJEucGIMRUZXUq3dLCuOqV81nQIjCh0PLK5"},
-	ProxyAddresses: []string{"185.186.27.185:15421", "185.186.27.184:15421"},
-	PairsList: [][]string{
-		{"bnbbidr", "bnbbrl", "bnbbusd", "bnbeur", "bnbrub", "bnbtry", "bnbtusd", "bnbuah", "bnbusdt", "bntusdt", "bnxusdt", "bondusdt", "bswtry", "bswusdt", "btcars", "btcbidr", "btcbrl", "btcbusd", "btcdownusdt", "btceur", "btcfdusd", "btcngn", "btcpln", "btcrub", "btctry", "btctusd", "btcuah", "btcusdc", "btcusdt", "btczar", "bttctry", "burgerusdt", "busdbidr", "busdbrl", "busdrub", "busdtry", "busduah", "busdusdt", "busdzar", "c98usdt", "cakeusdt", "celousdt", "celrusdt", "cfxtry", "cfxusdt", "chessusdt", "chrusdt", "chztry", "chzusdt", "citytry", "ckbusdt", "combotry", "combousdt", "comptry", "compusdt", "costry", "creambusd", "creamusdt", "crvusdt", "ctkusdt", "ctsiusdt", "ctxcusdt", "cvcbusd", "cvcusdt", "cybertry", "cyberusdt", "dartry", "darusdt", "dashusdt", "denttry", "dentusdt", "dgbusdt", "dodousdt", "dogebidr", "dogebusd", "dogetry", "dogeusdt", "dotrub", "dottry", "dotusdt", "drepusdt", "duskusdt", "dydxusdt", "edutry", "eduusdt", "egldusdt", "enjtry", "enjusdt", "ensusdt"},
-	},
+type GoCryptoTraderConfig struct {
+    APIKeys        []string `json:"apiKeys"`
+    ProxyAddresses []string `json:"proxyAddresses"`
+    EnabledPairs   []string `json:"enabledPairs"`
+    MaxPairs       int      `json:"maxPairs"`
 }
 
-var Instance3 = Config{
-	APIKeys:        []string{"jByRuDyDvM3bQl71hLgadWt932jodjvpJRqvXsQRIWHfpSZwxYBR7BWFBOXO7o6b", "ceox9ksHbEyMQiOkSkH0k2VnFWkiojJEucGIMRUZXUq3dLCuOqV81nQIjCh0PLK5"},
-	ProxyAddresses: []string{"185.186.27.185:15421", "185.186.27.184:15421"},
-	PairsList: [][]string{
-		{"eostry", "eosusdt", "epxusdt", "ernusdt", "etcusdt", "ethbidr", "ethbrl", "ethbusd", "etheur", "ethfdusd", "ethrub", "ethtry", "ethtusd", "ethusdc", "ethusdt", "ethzar", "eurusdt", "farmusdt", "fdusdbusd", "fdusdtry", "fdusdusdt", "fettry", "fetusdt", "fidausdt", "filbusd", "filtry", "filusdt", "flmusdt", "flokitry", "flokiusdt", "flowusdt", "fluxusdt", "forthbusd", "forthusdt", "fronttry", "frontusdt", "ftmtry", "ftmusdt", "fttbusd", "fttusdt", "fxsusdt", "galabusd", "galatry", "galausdt", "galtry", "galusdt", "gasusdt", "gftbusd", "gftusdt", "glmrusdt", "glmusdt", "gmttry", "gmtusdt", "gmxusdt", "grttry", "grtusdt", "gtcusdt", "hardusdt", "hbarusdt", "hftusdt", "hifiusdt", "highusdt", "hiveusdt", "hookusdt", "hottry", "hotusdt", "icptry", "icpusdt", "icxusdt", "idexusdt", "idtry", "idusdt", "imxusdt", "injtry", "injusdt", "iostusdt", "iotausdt", "iotxusdt", "jasmytry", "jasmyusdt", "joetry", "joeusdt", "jstusdt", "juvtry", "juvusdt", "kavausdt", "kdausdt", "keyusdt", "klayusdt"},
-	},
+// Предположим, что exchangePairsList обновляется при каждом запуске сервера
+var exchangePairsList = []string{
+    // ваш список валютных пар
 }
 
-var Instance4 = Config{
-	APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
-	ProxyAddresses: []string{"185.186.27.107:15421", "45.135.248.104:15421"},
-	PairsList: [][]string{
-		{"kncusdt", "kp3rusdt", "ksmusdt", "laziotry", "laziousdt", "ldousdt", "leverbusd", "leverusdt", "linausdt", "linkbusd", "linktry", "linkusdt", "litbusd", "litusdt", "lokausdt", "loomusdt", "lpttry", "lptusdt", "lqtyusdt", "lrctry", "lrcusdt", "lskusdt", "ltcbrl", "ltcbusd", "ltceur", "ltcrub", "ltctry", "ltcuah", "ltcusdt", "lunatry", "lunausdt", "luncbusd", "luncusdt", "magictry", "magicusdt", "manatry", "manausdt", "maskusdt", "maticbidr", "maticbrl", "maticbusd", "matictry", "maticusdt", "mavtry", "mavusdt", "mblusdt", "mboxtry", "mboxusdt", "mcusdt", "mdtusdt", "mdxusdt", "minatry", "minausdt", "mkrusdt", "mlnusdt", "movrusdt", "mtltry", "mtlusdt", "multibusd", "multiusdt", "nbtbidr", "neartry", "nearusdt", "neorub", "neotry", "neousdt", "nexousdt", "nknusdt", "nmrusdt", "oaxusdt", "oceantry", "oceanusdt", "ognbusd", "ogntry", "ognusdt", "ogtry", "ogusdt", "omgusdt", "onetry", "oneusdt", "ongusdt", "onttry", "ontusdt", "ookiusdt", "opbusd", "optry", "opusdt", "osmousdt", "oxtusdt"},
-	},
+func fillPairsFromDataFrame(instance *GoCryptoTraderConfig) {
+    if len(exchangePairsList) > instance.MaxPairs {
+        instance.EnabledPairs = exchangePairsList[:instance.MaxPairs]
+    } else {
+        instance.EnabledPairs = exchangePairsList
+    }
 }
 
-var Instance5 = Config{
-	APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
-	ProxyAddresses: []string{"185.186.27.107:15421", "45.135.248.104:15421"},
-	PairsList: [][]string{
-		{"paxgtry", "paxgusdt", "pendleusdt", "peopleusdt", "pepetry", "pepeusdt", "perlusdt", "perpusdt", "phausdt", "phbusdt", "plausdt", "pntusdt", "polsusdt", "polyxusdt", "pondusdt", "portotry", "powrusdt", "prosusdt", "psgtry", "pundixusdt", "pyrusdt", "qiusdt", "qkcusdt", "qntusdt", "qtumusdt", "quickusdt", "radtry", "radusdt", "rayusdt", "rdntusdt", "reeftry", "reefusdt", "reibusd", "reiusdt", "renusdt", "rifusdt", "rlcusdt", "rndrbusd", "rndrtry", "rndrusdt", "rosetry", "roseusdt", "rplusdt", "rsrusdt", "runebusd", "runeusdt", "rvntry", "rvnusdt", "sandtry", "sandusdt", "santostry", "santosusdt", "scrtusdt", "seitry", "seiusdt", "sfpusdt", "shibbusd", "shibdoge", "shibtry", "shibusdt", "slptry", "slpusdt", "snmbusd", "snxusdt", "solbidr", "solbrl", "solbusd", "soleur", "solrub", "soltry", "soltusd", "solusdt", "spelltry", "spellusdt", "srmbusd", "ssvusdt", "steemusdt", "stgusdt", "stmxbusd", "stmxusdt", "storjtry", "storjusdt", "stptusdt", "straxusdt", "stxtry", "stxusdt", "suitry", "suiusdt", "sushiusdt"},
-	},
-}
+var (
+    Instance1 = GoCryptoTraderConfig{
+        APIKeys:        []string{"jByRuDyDvM3bQl71hLgadWt932jodjvpJRqvXsQRIWHfpSZwxYBR7BWFBOXO7o6b", "ceox9ksHbEyMQiOkSkH0k2VnFWkiojJEucGIMRUZXUq3dLCuOqV81nQIjCh0PLK5"},
+        ProxyAddresses: []string{"185.186.27.185:15421", "185.186.27.184:15421"},
+        MaxPairs:       100,
+    }
+    Instance2 = GoCryptoTraderConfig{
+        APIKeys:        []string{"jByRuDyDvM3bQl71hLgadWt932jodjvpJRqvXsQRIWHfpSZwxYBR7BWFBOXO7o6b", "ceox9ksHbEyMQiOkSkH0k2VnFWkiojJEucGIMRUZXUq3dLCuOqV81nQIjCh0PLK5"},
+        ProxyAddresses: []string{"185.186.27.185:15421", "185.186.27.184:15421"},
+        MaxPairs:       100,
+    }
+    Instance3 = GoCryptoTraderConfig{
+        APIKeys:        []string{"jByRuDyDvM3bQl71hLgadWt932jodjvpJRqvXsQRIWHfpSZwxYBR7BWFBOXO7o6b", "ceox9ksHbEyMQiOkSkH0k2VnFWkiojJEucGIMRUZXUq3dLCuOqV81nQIjCh0PLK5"},
+        ProxyAddresses: []string{"185.186.27.185:15421", "185.186.27.184:15421"},
+        MaxPairs:       100,
+    }
+    Instance4 = GoCryptoTraderConfig{
+        APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
+        ProxyAddresses: []string{"185.186.27.107:15421", "45.135.248.104:15421"},
+        MaxPairs:       100,
+    }
+    Instance5 = GoCryptoTraderConfig{
+        APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
+        ProxyAddresses: []string{"185.186.27.107:15421", "45.135.248.104:15421"},
+        MaxPairs:       100,
+    }
+    Instance6 = GoCryptoTraderConfig{
+        APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
+        ProxyAddresses: []string{"185.186.27.107:15421", "45.135.248.104:15421"},
+        MaxPairs:       100,
+    }
+)
 
-var Instance6 = Config{
-	APIKeys:        []string{"cUe7mZpWMNi96Gunm57USrkstCaZOuOoR7qWzFHbAIjglh0wsAgCufwqQARHX59i", "AJaBKMzpGAyYeiP7hV4aN5IwuJL7W7RtExEdrxyBh6GUf875rIiS2Ek1iZIakx4C"},
-	ProxyAddresses: []string{"185.186.27.107:15421", "45.135.248.104:15421"},
-	PairsList: [][]string{
-		{"sxptry", "sxpusdt", "synusdt", "sysusdt", "tbusd", "thetausdt", "tkobidr", "tlmtry", "tlmusdt", "tomotry", "tomousdt", "trbbusd", "trbusdt", "truusdt", "trxbusd", "trxtry", "trxusdt", "tusdbusd", "tusdt", "tusdtry", "tusdusdt", "tvkusdt", "twttry", "twtusdt", "umatry", "unfitry", "unfiusdt", "uniusdt", "usdcusdt", "usdpusdt", "usdtars", "usdtbidr", "usdtbrl", "usdtdai", "usdtidrt", "usdtngn", "usdtpln", "usdtron", "usdtrub", "usdttry", "usdtuah", "usdtzar", "ustcusdt", "vettry", "vetusdt", "vgxusdt", "vibusdt", "vidtusdt", "voxelusdt", "vthousdt", "wavestry", "wavesusdt", "waxpusdt", "wbtcbusd", "wingusdt", "wldtry", "wldusdt", "woousdt", "wtcusdt", "xectry", "xecusdt", "xemusdt", "xlmtry", "xlmusdt", "xmrusdt", "xrpbidr", "xrpbrl", "xrpbusd", "xrpeur", "xrprub", "xrptry", "xrpusdt", "xtzusdt", "xvgtry", "xvgusdt", "xvstry", "xvsusdt", "yfiiusdt", "yfiusdt", "yggusdt", "zecusdt", "zenusdt", "zilbidr", "ziltry", "zilusdt", "zrxusdt"},
-	},
+func init() {
+    fillPairsFromDataFrame(&Instance1)
+    fillPairsFromDataFrame(&Instance2)
+    fillPairsFromDataFrame(&Instance3)
+    fillPairsFromDataFrame(&Instance4)
+    fillPairsFromDataFrame(&Instance5)
+    fillPairsFromDataFrame(&Instance6)
 }
